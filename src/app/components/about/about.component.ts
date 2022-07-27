5af92abb3d50e2cfc489fa9086bfd03bc5896ac9
gabrielagcantero <gabrielagcantero@gmail.com>
2022-07-26 21:31:43 -0300

    Conexión
    Traer cosas de la base de datos para mostrarlas en en front.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -12,7 +12,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data => {
-      this.myAbout=data.about;
+      this.myAbout=data;
     });
 
     
